7be304e4b94ca8d2205aaf60cffdcaaec75361e8
vinxenx0 <151790767+vinxenx0@users.noreply.github.com>
2026-09-12 13:54:09 +0200

    C6-F0.8-E4

--- tests/run_all.py
+++ tests/run_all.py
@@ -81,6 +81,7 @@ KNOWN_SUITES = {
     "C6F08ContentDefinitionDeterminismTest.gd" : "[C6F08_CONTENT_DEF_DETERMINISM_SUITE] PASS",
     "C6F08FractalPlaybackValidationTest.gd" : "[C6F08_FRACTAL_PLAYBACK_VALIDATION_SUITE] PASS",
     "C6F08VectorFieldPilotTest.gd" : "[C6F08_VECTOR_FIELD_PILOT_SUITE] PASS",
+    "C6F08SaccadePlaybackValidationTest.gd" : "[C6F08_SACCADE_PLAYBACK_VALIDATION_SUITE] PASS",
     "C6F08PursuitPlaybackValidationTest.gd" : "[C6F08_PURSUIT_PLAYBACK_VALIDATION_SUITE] PASS",
     "C6F08PeripheralScanPilotTest.gd" : "[C6F08_PERIPHERAL_SCAN_PILOT_SUITE] PASS",
     "C6F08TrackingPlaybackValidationTest.gd" : "[C6F08_TRACKING_PLAYBACK_VALIDATION_SUITE] PASS",
